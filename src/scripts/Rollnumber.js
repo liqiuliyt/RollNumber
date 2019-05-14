cbe86f2b23d64e3e3ecc0c69366bf77e510ab0d0
epub <epub@userdeMacBook-Pro.local>
2019-05-14 19:00:13 +0800

    update

--- src/scripts/Rollnumber.js
+++ src/scripts/Rollnumber.js
@@ -1,56 +1,48 @@
 // import { match } from "../../../../../Library/Caches/typescript/3.4.3/node_modules/@types/minimatch";
 
 class RollNumber {
-
-    constructor(min = 0, max = 100, autostart = false) {
-        this.min = min;
-        this.max = max;
-        this.autostart = autostart;
-        this.interval = null;
-        this.saveNunber = [];
+    constructor(optons) {
+        this.el = optons.el;
+        this.min = optons.min;
+        this.max = optons.max;
+        this.interval = optons.interval;
+        this.autostart = optons.autostart;
+        this.default = optons.default;
+        this.intervalName = null;
+        if (this.autostart) {
+            this.start();
+        }
     }
+
+
 
     start(value) {
-        
-        this.autostart=value;
+        this.el.innerText = this.default;
+        this.intervalName && clearInterval(this.intervalName);
         let allarray = [];
         let picknumber = [];
-       let _this=this;
-
-       if(this.autostart){
-        this.interval = setInterval(function () {
-            if (allarray.length > 0) {
-                picknumber = allarray[Math.floor(Math.random() * (allarray.length))];
-                    allarray.forEach((item, index) => {
-                        if (item == picknumber) {
-                            allarray.splice(index, 1);
-                        }
-                    });
-                    document.getElementById('content').innerText = picknumber;
-                } else {
-                    for (var i = _this.min; i <= _this.max; i++) {
-                        allarray.push(i);
-                        allarray.forEach((item, index) => {
-                            _this.saveNunber.forEach(element=>{
-                                if (element == item) {
-                                    allarray.splice(index, 1);
-                                }
-                            });
-                           
-                        }); 
-                    }
-                    
-                }
-        }, 100);
-    }
-    }
-    stop() {
-        clearInterval(this.interval);
-        this.saveNunber.push(document.getElementById('content').innerText);
-        document.getElementById('saveContent').innerText=this.saveNunber;
+        let _this = this;
+        this.intervalName = setInterval(function () {
+            _this.roll(allarray, picknumber);
+        }, this.interval);
     }
 
+    roll(allarray, picknumber) {
+        let _this = this;
+        for (var i = _this.min; i <= _this.max; i++) {
+            allarray.push(i);
+        }
+        picknumber = allarray[Math.floor(Math.random() * (allarray.length))];
+        this.el.innerText = picknumber;
+       
+    }
 
+    stop(n) {
+        clearInterval(this.intervalName);
+        if (n) {
+            this.el.innerText = n;
+        }
+    }
     reset() {
 
     }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,19 +6,38 @@ import pickNumber from './Picknumber';
 
 let start = document.getElementById('start');
 let stop = document.getElementById('stop');
-let pick=document.getElementById('pick');
+let reset = document.getElementById('reset');
 
-let rollnumber = new rollNumber(50,60,false);
-rollnumber.start();
-start.addEventListener('click',function(){
+let pick = document.getElementById('pick');
+
+let rollContent = document.getElementById('content');
+
+let options = {
+    'el': rollContent,
+    'min': 0,
+    'max': 100,
+    'autostart': true,
+    'interval': 100,
+    'default':0
+};
+
+let rollnumber = new rollNumber(options);
+let saveNunber=[];
+
+start.addEventListener('click', function () {
     rollnumber.start(true);
-},false);
-stop.addEventListener('click',function(){
-    rollnumber.stop();
-},false) ;
+}, false);
 
+stop.addEventListener('click', function () {
+    rollnumber.stop();
+    
+}, false);
+
+stop.addEventListener('reset', function () {
+     rollnumber.reset();
+}, false);
 
 var picknumber = new pickNumber();
-pick.addEventListener('click',function(){
+pick.addEventListener('click', function () {
     picknumber.pick();
-},false) ;
+}, false);
